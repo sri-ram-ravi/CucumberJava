59ba219ac017b4ad087a67d2ded7c8bfe5d0f5ff
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-06 11:53:12 +0530

    added before and after hooks on contact_us step definitions

--- CucumberJava/src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ CucumberJava/src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,34 +1,53 @@
 package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Contact_Us_Steps {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+    }
+
+    @After
+    public void tearDownDriver(){
+        driver.quit();
+    }
+
     @Given("Go to the webdriver university contact us page")
     public void go_to_the_webdriver_university_contact_us_page() {
-        
+        System.out.println("step 1-ku");
     }
     @When("I enter first name")
     public void i_enter_first_name() {
-        
+        System.out.println("step 2");
     }
     @When("I enter last name")
     public void i_enter_last_name() {
-        
+        System.out.println("step 3");
     }
     @When("I enter email address")
     public void i_enter_email_address() {
-        
+        System.out.println("step 4");
     }
     @When("I enter comment")
     public void i_enter_comment() {
-       
+        System.out.println("step 5");
     }
     @When("I click submit")
     public void i_click_submit() {
-        
+        System.out.println("step 6");
     }
     @Then("I should see {string}")
     public void i_should_see(String string) {
-        
+        System.out.println("step 7");
     }
 }
