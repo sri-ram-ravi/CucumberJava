a9745b56cee7a74828ba966c97e0a5728bd3fe3e
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-06 10:21:05 +0530

    added contact us steps

---
+++ CucumberJava/src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -0,0 +1,34 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class Contact_Us_Steps {
+    @Given("Go to the webdriver university contact us page")
+    public void go_to_the_webdriver_university_contact_us_page() {
+        
+    }
+    @When("I enter first name")
+    public void i_enter_first_name() {
+        
+    }
+    @When("I enter last name")
+    public void i_enter_last_name() {
+        
+    }
+    @When("I enter email address")
+    public void i_enter_email_address() {
+        
+    }
+    @When("I enter comment")
+    public void i_enter_comment() {
+       
+    }
+    @When("I click submit")
+    public void i_click_submit() {
+        
+    }
+    @Then("I should see {string}")
+    public void i_should_see(String string) {
+        
+    }
+}
